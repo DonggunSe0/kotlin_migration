c6ff906e9f0d3964ad6df3394c50637ffe3ee2d0
DonggunSe0 <cloudhenry1@gmail.com>
2026-04-30 12:21:50 +0900

    6-2

--- src/main/kotlin/wiseSaying/Rq.kt
+++ src/main/kotlin/wiseSaying/Rq.kt
@@ -1,32 +1,34 @@
 package wiseSaying
 
 class Rq(
-    val cmd: String,
+    cmd: String,
 ) {
 
-    fun getParam(paramName: String, defaultValue: String) : String{
+    private val paramMap: Map<String, String>
 
+    init {
         val cmdBits = cmd.split("?")
 
-        if(cmdBits.size < 2) {
-            return defaultValue
-        }
-
         val queryString = cmdBits[1]
+        paramMap = if (cmdBits.size == 2) {
+            queryString
+                .split("&")
+                .mapNotNull {
+                    val paramBits = it.split("=", limit = 2)
 
-        val paramMap = queryString
-            .split("&")
-            .mapNotNull{
-                val paramBits =it.split("=", limit = 2)
-                if(paramBits.size != 2) {
-                    null
-                }else{
-                    paramBits[0] to paramBits[1]
+                    if (paramBits.size != 2) {
+                        null
+                    } else {
+                        paramBits[0] to paramBits[1]
+                    }
                 }
-            }
-            .toMap()
+                .toMap()
+        } else {
+            emptyMap()
+        }
+    }
 
+    fun getParam(paramName: String, defaultValue: String): String {
         return paramMap[paramName] ?: defaultValue
-
     }
 }
