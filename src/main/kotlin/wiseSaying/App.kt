f788f38b35efd0eb17aa9ddf193726be1f83927d
DonggunSe0 <cloudhenry1@gmail.com>
2026-04-30 14:23:53 +0900

    10

--- src/main/kotlin/wiseSaying/App.kt
+++ src/main/kotlin/wiseSaying/App.kt
@@ -1,6 +1,7 @@
 package wiseSaying
 
-class App {
+class App( val wiseSayingController: WiseSayingController = WiseSayingController(),
+           val systemController: SystemController = SystemController()) {
     fun run(){
         var lastId = 0; // 수정이 필요해서 var로 선언
         val wiseSayings = mutableListOf<WiseSaying>()
@@ -10,63 +11,13 @@ class App {
 
             val input = readln()
             val rq = Rq(input)
-            when(input){
-                "종료" -> {
-                    println("프로그램을 종료합니다.")
-                    break
-                }
-                "등록" ->{
-                    print("명언 : ")
-                    val content = readln()
-                    print("작가 : ")
-                    val author = readln()
-                    val id = ++ lastId
+            when(rq.action){
+                "종료" -> systemController.exit()
+                "등록" -> wiseSayingController.write()
+                "목록" -> wiseSayingController.list()
+                "삭제" -> wiseSayingController.delete(rq)
 
-                    WiseSaying(id, content, author)
-                        .also {
-                            wiseSayings.add(it)
-                    }
-                    println("${id}번 명언이 등록되었습니다.")
-
-                }
-                "목록"->{
-                    println("번호 / 작가 / 명언")
-                    println("-".repeat(30))
-                    wiseSayings.reversed().forEach {
-                        println("${it.id}/${it.author}/${it.content}")
-                    }
-                }
-                "삭제"->{ //삭제?id=1
-                    val id = rq.getParamAsInt("id",0)
-
-                    if(id == 0){
-                        println("id를 입력해주세요")
-                        continue
-                    }
-
-                    val target = wiseSayings
-                        .firstOrNull{
-                            it.id == id
-                        }
-                    if(target == null){
-                        println("${id} 명언이 존재하지 않습니다.")
-                        continue
-                    }
-                    wiseSayings.remove(target)
-                    println("${id}번 명언이 삭제되었습니다.")
-
-//                  wiseSayings.removeIf { it.id == id } //표현의 간결성
-                   wiseSayings
-                       .firstOrNull{
-                        it.id == id
-                    }?.let {
-                        wiseSayings.remove(it)
-                    }//효율성
-                    ?: println("${id} 명언이 존재하지 않습니다.")
-
-                    println("${id}번 명언이 삭제되었습니다.")
                 }
             }
         }
     }
-}
